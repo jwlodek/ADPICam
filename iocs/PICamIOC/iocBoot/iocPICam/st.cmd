814ac0a09d5a85458d3bcec63884c30ae1f148e0
Jakub Wlodek <jwlodek@bnl.gov>
2022-06-22 13:12:28 -0400

    Modify the lib path

--- iocs/PICamIOC/iocBoot/iocPICam/st.cmd
+++ iocs/PICamIOC/iocBoot/iocPICam/st.cmd
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# Set the root location of your GENICAM installation
 export GENICAM_ROOT_V2_4=/opt/pleora/ebus_sdk/x86_64/lib/genicam
-export LD_LIBRARY_PATH=../../../../lib/linux-x86_64:$LD_LIBRARY_PATH
+
+# Set the library path.
+export LD_LIBRARY_PATH=../../../../PICamSupport/os/linux-x86_64:$LD_LIBRARY_PATH
 
 ../../bin/linux-x86_64/PICamApp st_base.cmd
